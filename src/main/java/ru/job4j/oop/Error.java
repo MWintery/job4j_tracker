bc2610dc761a83d0258fadf322b45f217a77d511
MWintery <marsi.sol807@gmail.com>
2023-11-23 23:47:02 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() { }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Состояние: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 507, "Insufficient Storage");
+        error.printInfo();
+        Error error1 = new Error();
+        error1.printInfo();
+        Error error2 = new Error(false, 0, "All ok!");
+        error2.printInfo();
+        Error error3 = new Error(true, 210, "Webpage Created");
+        error3.printInfo();
+    }
+}
