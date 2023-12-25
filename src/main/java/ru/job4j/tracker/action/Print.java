7b16ba06aa7579d9c8ff326b9ff5f67ea34c6157
MWintery <marsi.sol807@gmail.com>
2023-12-26 00:03:44 +0300

    Именование классов реализующих интерфейс [#505124]

--- src/main/java/ru/job4j/tracker/action/Print.java
+++ src/main/java/ru/job4j/tracker/action/Print.java
@@ -1,9 +1,14 @@
-package ru.job4j.tracker;
+package ru.job4j.tracker.action;
 
-public class PrintAction implements UserAction {
+import ru.job4j.tracker.input.Input;
+import ru.job4j.tracker.Item;
+import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.Tracker;
+
+public class Print implements User {
     private final Output output;
 
-    public PrintAction(Output output) {
+    public Print(Output output) {
         this.output = output;
     }
 
