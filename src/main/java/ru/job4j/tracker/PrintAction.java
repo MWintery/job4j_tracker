20cdd56047c6ddf61db6e1b1f80c5274199a6125
MWintery <marsi.sol807@gmail.com>
2023-12-25 17:23:06 +0300

    12.2. Тесты вывода на консоль [#33585]

---
+++ src/main/java/ru/job4j/tracker/PrintAction.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker;
+
+public class PrintAction implements UserAction {
+    private final Output output;
+
+    public PrintAction(Output output) {
+        this.output = output;
+    }
+
+    @Override
+    public String name() {
+        return "Показать все заявки";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        output.println("=== Показать все заявки ====");
+        Item[] items = tracker.findAll();
+        if (items.length > 0) {
+            for (Item item : items) {
+                output.println(item);
+            }
+        } else {
+            output.println("Хранилище еще не содержит заявок");
+        }
+        return true;
+    }
+}
